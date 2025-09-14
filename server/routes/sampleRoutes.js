cb21f9da5e3b3d9ad9718130a60f6aa9a8d2238e
MADHAV <madhavmahajan152@gmail.com>
2025-09-15 04:30:27 +0530

    dummy server deleted

--- server/routes/sampleRoutes.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-import express from 'express';
-const router = express.Router();
-const sampleController = require('../controllers/sampleController');
-const sampleMiddleware = require('../middleware/sampleMiddleware');
-
-router.get('/hello', sampleMiddleware, sampleController.sayHello);
-
-module.exports = router;
